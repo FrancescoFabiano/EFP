d7bed671acc20722ddfa068a3d274ee2d95c7e15
a-burigana <burigana.alessandro@gmail.com>
2020-03-24 14:30:31 +0100

    Implemented announcement actions in ASP.

---
+++ scripts/asp_print.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+DOMAIN="$1"      # Name of the domain
+shift            # Shift all arguments to the left (original $1 gets lost)
+ARGUMENTS=("$@") # Rebuild the array with rest of arguments
+ASP_PATH="../asp"
+OUT_PATH="../out/asp"
+
+clingo $ASP_PATH/planner_poss.lp $ASP_PATH/$DOMAIN >> $OUT_PATH/output.txt;
+python3 $ASP_PATH/output_render/out_reader.py $OUT_PATH/output;
+dot -Tpdf $OUT_PATH/output.dot >> $OUT_PATH/output.pdf;
+
+rm $OUT_PATH/output.txt;
+rm $OUT_PATH/output.dot;
