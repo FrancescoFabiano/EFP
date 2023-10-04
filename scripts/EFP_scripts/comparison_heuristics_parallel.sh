994a3519a090bfc0e71e4ab5d2a700428c0f4c69
Theo-Platt <theop@nmsu.edu>
2023-10-04 15:02:59 -0600

    updated heur_tests.sh to take -l argument which speeds up runtime by terminating slower heuristic threads when one finishes. Note: -l can only be supplied after -p has already been supplied.

--- scripts/EFP_scripts/comparison_heuristics_parallel.sh
+++ scripts/EFP_scripts/comparison_heuristics_parallel.sh
@@ -11,6 +11,32 @@ TIMEOUT="60";
 
 heuristics=("NONE" "S_PG" "L_PG" "C_PG" "SUBGOALS"); 
 
+cleanup(){
+  myPID=$BASHPID
+  # use mkdir as a mutex between the child processes
+  if mkdir lock.tmp
+  then
+    # when the mutex is aquired (this process finished first) kill all other processes doing heuristics
+    echo "$myPID has aquired the lock"
+    while IFS= read -r line; do
+      if [ $myPID -ne $line ]
+        then
+          echo "[$myPID] killing $line"
+          kill "$line" 
+      fi
+    done < pids.tmp
+    # unlock the mutex by destroying lock.tmp
+    rm -r lock.tmp
+  # process finished, but not first. wait to be killed. (wait instead of self terminating so that the pid remains taken and another process is not assigned it only to be killed by the first heuristic to complete.)
+  else  
+    echo "$BASHPID cannot aquire the lock."
+    sleep 5
+    echo "Process took to long to be killed, self terminating."
+    exit 0
+  fi
+  echo "$myPID has finished cleanup."
+}
+
 func(){
   # echo "1 is ${1}"
   # echo "1##*/ is ${1##*/}"
@@ -18,7 +44,12 @@ func(){
 
   file=$1
   heur=$2
-  tmp_output="findingplan_comparison_${heur}.tmp"
+  cleaner=$3
+  if [ $cleaner = "f" ]; then
+    tmp_output="findingplan_comparison_${heur}.tmp"
+  else
+    tmp_output="findingplan_comparison.tmp"
+  fi
   header=""
   body=""
   # echo "Working with file: $tmp_output"
@@ -26,6 +57,10 @@ func(){
 
   timeout $TIMEOUT bin/efp.out $file -results_file -check_visited -st POSS -h $heur > "$tmp_output"
   exit_status=$?;
+  if [ $cleaner = "t" ];then
+    cleanup &>/dev/null #silenced output
+  fi
+  
 
 	if [[ $exit_status -eq 124 ]]; then
         header="EFP Version 2.0 (on POSS and fast Bisimulation) with $heur and VISTED-STATE TIMED-OUT \n"
@@ -36,18 +71,25 @@ func(){
     body="$body \n [$heur]$(grep -w "Expanded Nodes:" "$tmp_output") \n"
     echo -e "$body" >> out/EFP_comparison/findingplan/"${1##*/}";
 	fi;
-    
-    # sleep 2s;
+  # sleep 2s;
   # rm "$tmp_output"
 }
 
+
+declare -a pids_array=()
+rm -r lock.tmp &>/dev/null #silenced output
+rm -rf pids.tmp &>/dev/null #silenced output
 for heur in "${heuristics[@]}"; do
-  echo "Spawning thread: $heur"
-  func $@ $heur &
+  func $1 $heur $2 &
+  echo "Spawned thread [$!]: $heur"
+  echo "$!" >> pids.tmp
   # sleep 5
 done;
 wait
 
-rm findingplan_comparison_*.tmp
+rm findingplan_comparison_*.tmp &>/dev/null #silenced output
+rm pids.tmp &>/dev/null #silenced output
+rm -r lock.tmp &>/dev/null #silenced output
+
 
 # cd /mnt/d/Documents/NMSU/Research/Parallel\ Programming/Codebase/EFP/
